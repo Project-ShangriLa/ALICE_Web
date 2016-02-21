295e0302bab593fc5985e8c97750fb3d8ff84ff9
AKB428 <gandara016+1@gmail.com>
2016-02-21 22:38:34 +0900

    グラフのロジックを追加

--- gen_csv_page.rb
+++ gen_csv_page.rb
@@ -32,7 +32,9 @@ head = <<"EOS"
 <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
-
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+<script src="./js/ex.js"></script>
 <script>
     $(function(){
         $("#csv_button").click(function () {
@@ -71,6 +73,7 @@ head = <<"EOS"
 
 </head>
 <body>
+<div style="float:left;">
 EOS
 
 body = ""
@@ -101,9 +104,14 @@ body += <<"EOS"
 <div>
         <button id="csv_button">CSVリンク生成</button><a href="" id="csv_link"></a>
 </div>
+<div>
         <button id="graph_button">グラフ生成</button>
         <p>2016年2月8日ぐらいからのデータがあります。</p>
     </div>
+</div>
+
+</div>
+<div id="container" style="min-width: 800px; height: 600px; margin: 0 auto; float:left;"></div>
 
 EOS
 
---
+++ js/ex.js
@@ -0,0 +1,114 @@
+$(function() {
+
+    function create_url(type) {
+
+        path = ""
+
+        if(type == "csv") {
+            path = "/data_csv"
+        } else if (type == "json") {
+            path = "/data_json"
+        }
+
+        var year = $('[name=year]').val();
+        var month = $('[name=month]').val();
+        var day = $('[name=day]').val();
+
+        month = ("0"+month).slice(-2);
+        day = ("0"+day).slice(-2);
+
+        var start = year + month + day
+
+        var _year = $('[name=_year]').val();
+        var _month = $('[name=_month]').val();
+        var _day = $('[name=_day]').val();
+
+        _month = ("0"+_month).slice(-2);
+        _day = ("0"+_day).slice(-2);
+
+        var end = _year + _month + _day;
+
+        console.log(start);
+        console.log(end);
+
+
+        //data_csv?ids=290,291&start=20160208&end=20160220
+
+        var chk_id_list = $('[class="target"]:checked').map(function(){return $(this).val();}).get();
+        var ids = chk_id_list.join(',');
+
+        if(type == "csv") {
+            $("#csv_link").text("ダウンロード!");
+            $("#csv_link").attr("href", path + "?ids=" + ids + "&" + "start=" + start + "&end=" + end );
+        } else if (type == "json") {
+            return path + "?ids=" + ids + "&" + "start=" + start + "&end=" + end
+        }
+
+    }
+
+
+    $("#graph_button").click(function () {
+
+        url = create_url("json");
+        console.log(url);
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: function(json) {
+                /*
+                        draw([{
+                            name: tagetAccountList[0],
+                            data: gdataList[0]
+                        }, {
+                            name: tagetAccountList[1],
+                            data: gdataList[1]
+                        }, ]);*/
+            }
+        });
+
+        function draw(series) {
+            $('#container').highcharts({
+                chart: {
+                    type: 'spline'
+                },
+                title: {
+                    text: 'pixiv 投稿増加数の日付別推移'
+                },
+                subtitle: {
+                    text: "今期アニメ"
+                },
+                xAxis: {
+                    type: 'category',
+                    /*
+                     dateTimeLabelFormats: {
+                     day: '%e. %b'
+                     },*/
+                    title: {
+                        text: '日付'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: '投稿数'
+                    },
+                    min: 0
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                },
+
+                plotOptions: {
+                    spline: {
+                        marker: {
+                            enabled: true
+                        }
+                    }
+                },
+
+                series: series
+            });
+        } //draw
+    });
+});
