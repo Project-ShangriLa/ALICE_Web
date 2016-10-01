55caf788fdb16515dd299433dae47f4aa87bd300
AKB428 <gandara016+1@gmail.com>
2016-10-01 18:14:39 +0900

    fix

--- save_twitter_icon.rb
+++ save_twitter_icon.rb
@@ -9,12 +9,12 @@ def save_image(url, download_file_path)
   File.open(download_file_path, 'wb') { |fp| fp.write(response.body) }
 end
 
-def connect_twitter(account_list)
+def connect_twitter(account_list, image_type = '')
   require './twitter.rb'
   puts 'id,twitter_profile_image_url'
  @tw.users(account_list).each do |user|
    #http://d.hatena.ne.jp/nakamura001/20111129/1322579169
-   image_url = user.profile_image_url.to_s.gsub(/_normal/,'')
+   image_url = user.profile_image_url.to_s.gsub(/_normal/, image_type)
    ext = File.extname(image_url)
    filename = './html/image/twitter_icon_' + @master_map[user.screen_name].to_s + ext
 
@@ -28,6 +28,8 @@ end
 
 master = Shangrila::Sora.new().get_master_data(ARGV[0], ARGV[1])
 
+image_type = ARGV[2] #_bigger
+
 @master_map = {}
 account_list = []
 
@@ -36,4 +38,4 @@ master.each do |m|
   account_list << m['twitter_account']
 end
 
-connect_twitter(account_list)
+connect_twitter(account_list, image_type)
